26546b5ccf627293637d57f0a4fb953330c7f360
eli-cool <eh128257@student.wpsok.org>
2026-01-06 23:44:36 -0600

    Add files via upload
    healthbar

--- heart/scripts/player.gd
+++ heart/scripts/player.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
+@export var hp := 100
+@export var speed := 200.0
+@export var is_hurting := false
+@onready var healthbar = $"../healthbar"
 
-@export var speed := 150.0
+
+func ready():
+	healthbar.value = hp
+
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -11,3 +18,25 @@ func _physics_process(_delta):
 	velocity = direction * speed
 	
 	move_and_slide()
+	
+	#hurting
+	if is_hurting:
+		print(hp)
+		healthbar.value = hp
+		hp -= 1
+	if hp <= 0:
+		queue_free()
+	
+	
+func _on_area_2d_area_entered(area: Area2D) -> void:
+#	print(area.get_parent())
+	if area.get_parent().is_in_group("attack"):
+		is_hurting = true
+#		queue_free()
+		print("ouch")
+
+
+func _on_area_2d_area_exited(area):
+	if area.get_parent().is_in_group("attack"):
+		is_hurting = false
+		print("no more ouch")
